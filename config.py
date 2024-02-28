610b350c0746887f060186fe0bce782bc157dc8e
Harsha <158590882+Harsh-un@users.noreply.github.com>
2024-02-28 21:04:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "26516344")) #optional
+API_HASH = getenv("API_HASH", "7a3f7d55d89476a15a62b4dd39062556") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
