fc60058f3ce02d728686aa55d728910070c15ceb
Harsha <158590882+Harsh-un@users.noreply.github.com>
2024-02-28 18:32:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,15 @@ OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/29a06fa7be4286c403f73.jpg')
-ALIVE_TEXT = getenv("ALIVE_TEXT")
+ALIVE_TEXT = getenv("ALIVE_TEXT", '✘ υη υѕєявσт ✘
+
+❏ νєяѕισи: 2.1
+├• υρтιмє: {uptime}
+├• ρутнσи: 3.9.7
+├• ρуяσgяαм: 2.0.106
+├• ѕυρρσят: [Click](https://t.me/un_bots)
+├• ¢нαииєℓ: [Click](https://t.me/un_bots_info)
+└• яєρσ: [Click](https://t.me/harsh_un)')
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
